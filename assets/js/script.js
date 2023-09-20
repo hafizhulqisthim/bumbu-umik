31099e60daec7f87157faa120f4413b3ac82ed58
andikatuluspangestu <andikatulusp@gmail.com>
2023-09-20 08:59:12 +0700

    Update : Add all sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 // Load all sections
 $(document).ready(function () {
+  // Load all regular sections
   $("#header").load("partials/header.html");
-  $("#main").load("partials/main.html");
   $("#footer").load("partials/footer.html");
+
+  // Load all contents sections
+  $("#daftar-produk").load("partials/contents/daftar-produk.html");
+  $("#produk-unggulan").load("partials/contents/produk-unggulan.html");
+  $("#tentang-kami").load("partials/contents/tentang-kami.html");
+  $("#testimonial").load("partials/contents/testimonial.html");
 });
 
 // AOS Initialization
